82e2ac7e2157133427830053cddd4d6bb26aab5a
danembb <dbrophy720@gmail.com>
2022-06-07 13:56:49 -0600

    Refactor routes to account for versioning

---
+++ app/controllers/api/v1/investigators_controller.rb
@@ -0,0 +1,8 @@
+class InvestigatorsController < ApplicationController
+
+  # GET /investigators or /investigators.json
+  def index; end
+
+  # GET /investigators/1 or /investigators/1.json
+  def show; end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,15 +1,22 @@
 Rails.application.routes.draw do
   
   root 'welcome#index'
+  
+  #API-Related routes
+  namespace :api do
+    namespace :v1 do
+      resources :investigators, only: [:index, :show]
+    end
+  end
 
+  #Routes via resources (mostly)
   resources :users, only: [:create, :delete] do
     resources :teams, only: [:create, :new, :edit, :update, :destroy]
     get "/dashboard", to: "users#show"
     post "/dashboard", to: "users#show"
   end
   
-  resources :investigators, only: [:index, :show]
-
+  #Routes via handrolling for Auth
   get "/registration", to: "users#new"
   get '/login', to: 'sessions#new'
   post '/login', to: 'sessions#create'
