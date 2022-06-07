eb5fd0eee102e1caf422122783d5262d36ac6366
danembb <dbrophy720@gmail.com>
2022-06-06 23:44:13 -0600

    Add dashboard  view with limited team functionality

--- app/controllers/teams_controller.rb
+++ app/controllers/teams_controller.rb
@@ -1,5 +1,5 @@
 class TeamsController < ApplicationController
-  before_action :set_team, only: %i[ show edit update destroy ]
+  # before_action :set_team, only: %i[ show edit update destroy ]
 
   # GET /teams or /teams.json
   def index
@@ -7,64 +7,30 @@ class TeamsController < ApplicationController
   end
 
   # GET /teams/1 or /teams/1.json
-  def show
-  end
+  def show; end
 
   # GET /teams/new
-  def new
-    @team = Team.new
-  end
+  def new; end
 
   # GET /teams/1/edit
-  def edit
-  end
+  def edit; end
 
   # POST /teams or /teams.json
   def create
+    @user = current_user
     @team = Team.new(team_params)
-
-    respond_to do |format|
-      if @team.save
-        format.html { redirect_to team_url(@team), notice: "Team was successfully created." }
-        format.json { render :show, status: :created, location: @team }
-      else
-        format.html { render :new, status: :unprocessable_entity }
-        format.json { render json: @team.errors, status: :unprocessable_entity }
-      end
-    end
   end
 
   # PATCH/PUT /teams/1 or /teams/1.json
-  def update
-    respond_to do |format|
-      if @team.update(team_params)
-        format.html { redirect_to team_url(@team), notice: "Team was successfully updated." }
-        format.json { render :show, status: :ok, location: @team }
-      else
-        format.html { render :edit, status: :unprocessable_entity }
-        format.json { render json: @team.errors, status: :unprocessable_entity }
-      end
-    end
-  end
+  def update; end
 
   # DELETE /teams/1 or /teams/1.json
-  def destroy
-    @team.destroy
-
-    respond_to do |format|
-      format.html { redirect_to teams_url, notice: "Team was successfully destroyed." }
-      format.json { head :no_content }
-    end
-  end
+  def destroy; end
 
   private
-    # Use callbacks to share common setup or constraints between actions.
-    def set_team
-      @team = Team.find(params[:id])
-    end
 
     # Only allow a list of trusted parameters through.
     def team_params
-      params.require(:team).permit(:name, :campaign, :active)
+      params.permit(:name, :campaign, :active)
     end
 end
--- spec/features/users/dashboard_spec.rb
+++ spec/features/users/dashboard_spec.rb
@@ -3,7 +3,7 @@ require "rails_helper"
 RSpec.describe "user dashboard page" do
   before :each do
     visit registration_path
-    fill_in :username, with: "nicosqueako"
+    fill_in :username, with: "nico_squeako"
     fill_in :password, with: "alsomeow"
     fill_in :password_confirmation, with: "alsomeow"
 
@@ -19,5 +19,18 @@ RSpec.describe "user dashboard page" do
     expect(page).to have_content(expected)  
   end
 
+  it "can display button to create a new team that can navigate to new team page" do
+    expect(page).to have_link("Create A Team")
 
+    click_on "Create A Team"
+
+    expect(current_path).to eq(new_user_team_path(@user))
+  end
+
+  xit "can display a users teams" do
+
+    within "#team-pane" do
+        expect(page).to have_content(@user.team)
+    end
+  end
 end
