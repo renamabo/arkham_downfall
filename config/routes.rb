12ea558a727839d27d387b50d3a0dbdf9ea5f687
Dane Brophy <76824096+danembb@users.noreply.github.com>
2022-06-06 23:53:43 -0600

    Revert "Dashboard page"

--- config/routes.rb
+++ config/routes.rb
@@ -2,16 +2,17 @@ Rails.application.routes.draw do
   
   root 'welcome#index'
 
+  resources :investigators do
+    resources :teams
+  end
+  
   resources :users, only: [:create, :delete] do
-    resources :teams, only: [:create, :new, :edit, :update, :destroy]
     get "/dashboard", to: "users#show"
     post "/dashboard", to: "users#show"
   end
   
-  resources :investigators, only: [:index, :show]
-
   get "/registration", to: "users#new"
+  
   get '/login', to: 'sessions#new'
   post '/login', to: 'sessions#create'
-
 end
