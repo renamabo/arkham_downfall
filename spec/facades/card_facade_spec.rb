7148579111fe63a853dd7cf6c0d2ed3ce1d37e53
danembb <dbrophy720@gmail.com>
2022-06-17 12:16:06 -0600

    Update card_facade testing after poro implementation

--- spec/facades/card_facade_spec.rb
+++ spec/facades/card_facade_spec.rb
@@ -6,5 +6,7 @@ RSpec.describe "card facade", :vcr do
 
         expect(cards).to be_an(Array)
         expect(cards.first).to be_a(Card)
+        expect(cards.first.name).to be_a(String)
+        expect(cards.count).to eq(1361)
     end
 end
